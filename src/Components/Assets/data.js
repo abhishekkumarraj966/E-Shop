75910bdb578d01f8db250802f7cd42dde346cc60
abhishekkumarraj966 <abhishekkumarraj62073@gmail.com>
2023-12-11 01:14:42 +0530

    Don

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,7 +1,7 @@
 import p1_img from './product_1.png'
-import p2_img from './product_2.png'
 import p3_img from './product_3.png'
-import p4_img from './product_4.png'
+import p26_img from "./product_26.png";
+import p20_img from "./product_20.png";
 
 let data_product = [
   {
@@ -11,11 +11,13 @@ let data_product = [
     new_price:50.00,
     old_price:80.50,
   },
-  {id:2,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image:p2_img,
-    new_price:85.00,
-    old_price:120.50,
+  {
+    id: 2,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    category: "kids",
+    image: p26_img,
+    new_price: 85.0,
+    old_price: 120.5,
   },
   {id:3,
     name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
@@ -23,11 +25,13 @@ let data_product = [
     new_price:60.00,
     old_price:100.50,
   },
-  {id:4,
-    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image:p4_img,
-    new_price:100.00,
-    old_price:150.00,
+  {
+    id: 4,
+    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    category: "mens",
+    image: p20_img,
+    new_price: 85.0,
+    old_price: 120.5,
   },
 ];
 
